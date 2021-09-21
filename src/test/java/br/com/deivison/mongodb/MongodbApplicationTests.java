655c0179c03b58dd6ffccaafd32cbcaecf9e04b3
Deivison V D Reis <deivisomreis@gmail.com>
2021-09-21 18:43:14 -0300

    estrutura inicial do projeto com mongodb

---
+++ src/test/java/br/com/deivison/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deivison.mongodb;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
